4cbff18b531b5349dfa6ccb4bbdf7c31f50390d0
Polycarp Okock <polycarpokock@live.com>
2019-03-01 12:55:12 +0100

    Added enums for storing, loading raw files

--- ImageInLib/ImageInLib/include/file.h
+++ ImageInLib/ImageInLib/include/file.h
@@ -6,11 +6,13 @@
 
 typedef enum {
 	LOAD_DATA_VTK = 1,
-	STORE_DATA_VTK
+	LOAD_DATA_RAW,
+	STORE_DATA_VTK,
+	STORE_DATA_RAW
 } operationType;
 
 bool manageFile(void  ** imageDataPtr, const size_t length, const size_t width,
-	const size_t height, unsigned char * pathPtr, VTKHeaderLines *lines, operationType operation);
+	const size_t height, unsigned char * pathPtr, VTKHeaderLines *lines, operationType operation, loadDataType dType);
 
 /*Converts to doulbe*/
 void convertTodataType(unsigned char ** dataPtrUC, dataType ** dataPtrD, const size_t dimXY, const size_t height);
