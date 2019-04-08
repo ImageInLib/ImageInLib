a225a5c2af29f4a630dc57ee0064f178e0789328
Polycarp Okock <pokock@aims.ac.tz>
2019-04-08 14:59:22 +0200

    removing unnecessary part

--- ImageInLib/utils/PerformanceAnalyzer/src/FormatTime.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <atlstr.h>
-
-static const int DefaultIntervalPrecision = 3;
-static const int DefaultPrecisionAfterSeconds = 7;
-
-CString FormatTimeInterval(double dTime, int precision);
-CString FormatAbsoluteTime(double dTime, int precisionAfterSeconds = 0);
