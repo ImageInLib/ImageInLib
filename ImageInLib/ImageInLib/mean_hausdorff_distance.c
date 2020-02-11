58834a57449ba3cb92c4b1b9c575b8b8033571f2
Polycarp Okock <deaspo@users.noreply.github.com>
2020-02-11 09:24:27 +0100

    Blank mhd fn body

--- ImageInLib/ImageInLib/mean_hausdorff_distance.c
+++ ImageInLib/ImageInLib/mean_hausdorff_distance.c
@@ -1,4 +1,12 @@
 //==============================================================================
 #include "mean_hausdorff_distance.h"
 //==============================================================================
+#include <float.h>
 //==============================================================================
+
+dataType mean_hausdorff(dataType ** curveA_Pointer, dataType curveB_Pointer, dataType curveIntensity, size_t height, size_t length, size_t width)
+{
+	dataType mhd = DBL_MAX;
+
+	return mhd;
+}
--- ImageInLib/ImageInLib/mean_hausdorff_distance.h
+++ ImageInLib/ImageInLib/mean_hausdorff_distance.h
@@ -1,1 +1,25 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+	//==============================================================================
 #pragma once
+#ifndef MHD
+#define MHD
+	//==============================================================================
+	/*
+	* Header file to contain general functions, variables, structs
+	* These can be used to calculate the mean hausdorff distance
+	* Avoids redefinitions elsewhere
+	*/
+	//==============================================================================
+	// Includes
+#include "common_functions.h"
+	//==============================================================================
+	dataType mean_hausdorff(dataType ** curveA_Pointer, dataType curveB_Pointer, dataType curveIntensity, size_t height, size_t length, size_t width);
+	//==============================================================================
+#endif // !MHD
+
+
+#ifdef __cplusplus
+}
+#endif
