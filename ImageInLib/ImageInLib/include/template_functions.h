302a550fc855e5ab4c9275abb1efc745c8856380
Konan Allaly <konan.allaly@tatramed.sk>
2022-08-12 19:28:40 +0200

    change from 1D array to 2D array

--- ImageInLib/ImageInLib/include/template_functions.h
+++ ImageInLib/ImageInLib/include/template_functions.h
@@ -1,14 +1,14 @@
 #pragma once
 
-//#include<iostream>
+#include<iostream>
 
 template <typename T>
 bool store3dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr);
 
 template<typename T>
-inline bool store3dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr)
+inline bool store3dRawData(T** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr)
 {
-	size_t i, j, k, xd;
+	size_t i, j, k;
 	FILE* file;
 
 	//checks if the memory was allocated
@@ -25,9 +25,7 @@ inline bool store3dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim
 		{
 			for (j = 0; j < yDim; j++)
 			{
-				xd = x_flat(i, j, k, xDim, yDim);
-
-				fwrite(&imageDataPtr[xd], sizeof(T), 1, file);
+				fwrite(&imageDataPtr[k][x_new(i, j, xDim)], sizeof(T), 1, file);
 			}
 		}
 	}
