4e4b8e52911bf4d7b4158d0b6159112a59acd9db
Konan Allaly <konan.allaly@tatramed.sk>
2022-08-12 17:00:07 +0200

    Template functions added

---
+++ ImageInLib/ImageInLib/include/template_functions.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//#include<iostream>
+
+template <typename T>
+bool store3dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr);
+
+template<typename T>
+inline bool store3dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr)
+{
+	size_t i, j, k, xd;
+	FILE* file;
+
+	//checks if the memory was allocated
+	if (imageDataPtr == NULL)
+		return false;
+
+	//writing binary data to file
+	if (fopen_s(&file, pathPtr, "wb") != 0)
+		return false;
+
+	for (k = 0; k < zDim; k++)
+	{
+		for (i = 0; i < xDim; i++)
+		{
+			for (j = 0; j < yDim; j++)
+			{
+				xd = x_flat(i, j, k, xDim, yDim);
+
+				fwrite(&imageDataPtr[xd], sizeof(T), 1, file);
+			}
+		}
+	}
+
+	fclose(file);
+
+	return true;
+}
