c7358fea4085975715b030b0589adadefd045286
Konan Allaly <konan.allaly@tatramed.sk>
2022-08-17 09:28:57 +0200

    removal of first version of 2D/3D labelling functions

--- ImageInLib/ImageInLib/include/Labelling.h
+++ ImageInLib/ImageInLib/include/Labelling.h
@@ -7,8 +7,6 @@ extern "C" {
 
 
 	//for 2D image using 2D arrays
-	bool regionLabelling2D(int** imageDataPtr, int** segmentedImage, const size_t xDim, const size_t yDim, int background, int object);
-
 	bool labelling2D(int** imageDataPtr, int** segmentedImage, bool** statusArray, const size_t xDim, const size_t yDim, int object);
 
 	//Statistics after labelling
@@ -27,13 +25,8 @@ extern "C" {
 	bool sortArray(int* valuePtr, int sizeArray);
 	//=============================
 
-	//for 3D images
-	bool fixEquivalence(int** segmentedImage, const size_t xDim, size_t x, size_t y, size_t z, int minV, int maxV, bool parallize, size_t nbtreads);
-
-	bool regionLabelling3D(dataType** imageDataPtr, int** segmentedImage, const size_t xDim, const size_t yDim, const size_t zDim, dataType background, dataType object, bool parallize, size_t nbtreads);
-
+	//For 3D Images
 	bool labelling3D(dataType** imageDataPtr, int** segmentedImage, bool** statusArray, const size_t xDim, const size_t yDim, const size_t zDim, dataType object);
-
 
 
 #ifdef __cplusplus
