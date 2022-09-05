28cd67b45762c619adf167879d13eaf0b2c7c993
Konan Allaly <konan.allaly@tatramed.sk>
2022-09-05 14:55:13 +0200

    updates of function for saving

--- ImageInLib/ImageInLib/src/data_storage.c
+++ ImageInLib/ImageInLib/src/data_storage.c
@@ -79,6 +79,7 @@ bool store3dDataVtkUC(unsigned char ** array3DPtr, const size_t xDim, const size
 bool store3dDataArrayD(dataType ** array3DPtr, const size_t xDim, const size_t yDim,
 	const size_t zDim, unsigned char * pathPtr, Storage_Flags flags)
 {
+	size_t k, i, j;
 	const size_t dimXY = xDim * yDim;
 	FILE *cfPtr;
 
@@ -101,25 +102,24 @@ bool store3dDataArrayD(dataType ** array3DPtr, const size_t xDim, const size_t y
 
 	if (flags.revertDataBytes)
 	{
-		for (size_t k = 0; k < zDim; k++)
+		for (k = 0; k < zDim; k++)
 		{
-			for (size_t j = 0; j < dimXY; j++)
+			for (i = 0; i < dimXY; i++)
 			{
-				//double tmp = array3DPtr[k][j];  //--> initial code
-				dataType tmp = array3DPtr[k][j];
-				//revertBytes(&tmp, sizeof(double)); //--> initial code
+				dataType tmp = array3DPtr[k][i];
 				revertBytes(&tmp, sizeof(dataType));
-				//fwrite(&tmp, sizeof(double), 1, cfPtr); //--> initial code
-				fwrite(&tmp, sizeof(dataType), 1, cfPtr);
+				fwrite(&tmp, sizeof(dataType), dimXY, cfPtr);
 			}
 		}
 	}
 	else
 	{
-		for (size_t k = 0; k < zDim; k++)
-		{
-			//fwrite(array3DPtr[k], sizeof(double), dimXY, cfPtr); //--> initial code
-			fwrite(array3DPtr[k], sizeof(dataType), dimXY, cfPtr);
+		for (k = 0; k < zDim; k++) {
+			for (i = 0; i < xDim; i++) {
+				for (j = 0; j < yDim; j++) {
+					fwrite(&array3DPtr[k][x_new(i, j, xDim)], sizeof(dataType), 1, cfPtr);
+				}
+			}
 		}
 	}
 
