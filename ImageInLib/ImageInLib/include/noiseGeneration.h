218ac1dd1bcf877c167c96994ddd9a0206f5d5c4
Konan Allaly <konan.allaly@tatramed.sk>
2022-09-06 15:41:59 +0200

    add structural noise in enum

--- ImageInLib/ImageInLib/include/noiseGeneration.h
+++ ImageInLib/ImageInLib/include/noiseGeneration.h
@@ -9,11 +9,13 @@ extern "C" {
 	{
 		SALT_AND_PEPPER = 1,
 		ADDITIVE_NOISE = 2,
-		MULTIPLICATIVE_NOISE = 3
+		MULTIPLICATIVE_NOISE = 3,
+		STRUCTURAL_NOISE = 4
 	} NoiseType;
 
-	void addNoiseToImage(dataType ** array3DPtr, const size_t xDim, const size_t yDim, const size_t zDim,
-		float density, const NoiseType method, dataType pepper); //adding of pepper
+	void addNoiseToImage(dataType** array3DPtr, const size_t xDim, const size_t yDim, const size_t zDim,
+		NoiseParameters parameters, const NoiseType method);
+
 
 #ifdef __cplusplus
 }
