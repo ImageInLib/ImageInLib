063f2b31318b3831739fe66b107c38dc10990431
Konan Allaly <konan.allaly@tatramed.sk>
2022-09-06 13:44:22 +0200

    conflict

--- ImageInLib/ImageInLib/src/noiseGeneration.c
+++ ImageInLib/ImageInLib/src/noiseGeneration.c
@@ -4,18 +4,21 @@
 
 // length == xDim, width == yDim, height == zDim
 void addNoiseToImage(dataType ** array3DPtr, const size_t xDim, const size_t yDim, const size_t zDim,
-	float density, const NoiseType method, dataType pepper)
+	dataType density, const NoiseType method)
 {
 	switch (method)
 	{
 	case SALT_AND_PEPPER:
-		saltAndPepper3dNoise_D(array3DPtr, xDim, yDim, zDim, density, pepper);
+		saltAndPepper3dNoise_D(array3DPtr, xDim, yDim, zDim, density);
 		break;
 	case ADDITIVE_NOISE:
-		additive3dNoise_D(array3DPtr, xDim, yDim, zDim, density);
+		additive3dNoise_D(array3DPtr, xDim, yDim, zDim, parameters.additive_value, parameters.fgMin, parameters.bgMax);
 		break;
 	case MULTIPLICATIVE_NOISE:
-		addMultiplicativeNoise(array3DPtr, zDim, yDim, density);
+		addMultiplicativeNoise(array3DPtr, zDim, xDim, yDim, parameters.multiplicative_variance, parameters.fgMin, parameters.bgMax);
+		break;
+	case STRUCTURAL_NOISE:
+		addStructuralNoise(array3DPtr, zDim, xDim, yDim, parameters.fgMin, parameters.bgMax);
 		break;
 	default:
 		break;
