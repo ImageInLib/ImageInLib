8b0dc72b68c937cbe704111aee439bb8f84e0572
Konan Allaly <konan.allaly@tatramed.sk>
2022-09-27 13:23:27 +0200

    update morphology

--- ImageInLib/ImageInLib/include/morphological_change.h
+++ ImageInLib/ImageInLib/include/morphological_change.h
@@ -9,12 +9,12 @@ extern "C" {
 	//Erosion removes floating pixels and thin lines so that only substantive objects remain
 	bool erosion3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background);
 
-	bool erosion3dSixNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background);
+	bool erosion3dHeighteenNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background);
 
 	//Dilation makes objects more visible and fills in small holes in objects.
 	bool dilatation3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background);
 
-
+	bool dilatation3dHeighteenNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background);
 #ifdef __cplusplus
 }
 #endif
--- ImageInLib/ImageInLib/src/morphological_change.c
+++ ImageInLib/ImageInLib/src/morphological_change.c
@@ -15,32 +15,25 @@ bool erosion3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim, co
 	size_t i, j, k, cpt, dim2d = xDim * yDim;
 	size_t xDim_ext = xDim + 2, yDim_ext = yDim + 2, zDim_ext = zDim + 2, dim2d_ext = xDim_ext * yDim_ext;
 
-	dataType** maskImage = (dataType**)malloc(zDim * sizeof(dataType*));
-	for (k = 0; k < zDim; k++)
-		maskImage[k] = (dataType*)malloc(dim2d * sizeof(dataType));
-	if (maskImage == NULL) return false;
-	//Initialization
-	for (k = 0; k < zDim; k++) {
-		for (i = 0; i < xDim; i++) {
-			for (j = 0; j < yDim; j++) {
-				maskImage[k][x_new(i, j, xDim)] = background;
-			}
-		}
-	}
-
 	dataType** extendedImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
-	for (k = 0; k < zDim_ext; k++)
+	dataType** maskImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
+	for (k = 0; k < zDim_ext; k++) {
 		extendedImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
-	if (extendedImage == NULL) return false;
+		maskImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
+	}
+	if (extendedImage == NULL || maskImage == NULL) return false;
+
 	//Initialization
 	for (k = 0; k < zDim_ext; k++) {
 		for (i = 0; i < xDim_ext; i++) {
 			for (j = 0; j < yDim_ext; j++) {
 				extendedImage[k][x_new(i, j, xDim_ext)] = background;
+				maskImage[k][x_new(i, j, xDim_ext)] = background;
 			}
 		}
 	}
 	copyDataToExtendedArea(imageDataPtr, extendedImage, zDim, xDim, yDim);
+	copyDataToExtendedArea(imageDataPtr, maskImage, zDim, xDim, yDim);
 	reflection3D(extendedImage, zDim_ext, xDim_ext, yDim_ext);
 
 	//neigbors check
@@ -131,39 +124,25 @@ bool erosion3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim, co
 					cpt++;
 				}
 
-				if (cpt == 26) {
-					maskImage[k - 1][x_new(i - 1, j - 1, xDim)] = object;
+				if (cpt < 26) {
+					maskImage[k][x_new(i, j, xDim_ext)] = background;
 				}
-				else {
-					maskImage[k - 1][x_new(i - 1, j - 1, xDim)] = background;
-				}
-
 			}
 		}
 	}
-
-	//copy
-	for (k = 0; k < zDim; k++) {
-		for (i = 0; i < xDim; i++) {
-			for (j = 0; j < yDim; j++) {
-				imageDataPtr[k][x_new(i, j, xDim)] = maskImage[k][x_new(i, j, xDim)];
-			}
-		}
-	}
+	copyDataToReducedArea(imageDataPtr, maskImage, zDim, xDim, yDim);
 
 	//clean memory
-	for (k = 0; k < zDim; k++)
+	for (k = 0; k < zDim_ext; k++) {
+		free(extendedImage[k]); 
 		free(maskImage[k]);
-	free(maskImage);
-
-	for (k = 0; k < zDim + 2; k++)
-		free(extendedImage[k]);
-	free(extendedImage);
+	}
+	free(extendedImage); free(maskImage);
 
 	return true;
 }
 
-bool erosion3dSixNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background) {
+bool erosion3dHeighteenNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background) {
 	if (imageDataPtr == NULL)
 		return false;
 
@@ -187,20 +166,39 @@ bool erosion3dSixNeigbours(dataType** imageDataPtr, const size_t xDim, const siz
 		}
 	}
 	copyDataToExtendedArea(imageDataPtr, extendedImage, zDim, xDim, yDim);
+	copyDataToExtendedArea(imageDataPtr, maskImage, zDim, xDim, yDim);
 	reflection3D(extendedImage, zDim_ext, xDim_ext, yDim_ext);
-	copyDataToAnotherArray(extendedImage, maskImage, zDim_ext, xDim_ext, yDim_ext);
 
-	//neigbors check ---> 6 neigbors
+	//neigbors check ---> 18 neigbors
 	for (k = 1; k < zDim_ext - 1; k++) {
 		for (i = 1; i < xDim_ext - 1; i++) {
 			for (j = 1; j < yDim_ext - 1; j++) {
 
 				cpt = 0;
 
+				if (extendedImage[k - 1][x_new(i - 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
 				if (extendedImage[k - 1][x_new(i, j, xDim_ext)] == object) {
 					cpt++;
 				}
+				if (extendedImage[k - 1][x_new(i, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+
+				if (extendedImage[k][x_new(i - 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
 				if (extendedImage[k][x_new(i - 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i - 1, j + 1, xDim_ext)] == object) {
 					cpt++;
 				}
 				if (extendedImage[k][x_new(i, j - 1, xDim_ext)] == object) {
@@ -209,15 +207,34 @@ bool erosion3dSixNeigbours(dataType** imageDataPtr, const size_t xDim, const siz
 				if (extendedImage[k][x_new(i, j + 1, xDim_ext)] == object) {
 					cpt++;
 				}
+				if (extendedImage[k][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
 				if (extendedImage[k][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i + 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+
+				if (extendedImage[k + 1][x_new(i - 1, j + 1, xDim_ext)] == object) {
 					cpt++;
 				}
 				if (extendedImage[k + 1][x_new(i, j, xDim_ext)] == object) {
 					cpt++;
 				}
+				if (extendedImage[k + 1][x_new(i, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
 
-				if (cpt < 6) {
-					maskImage[k][x_new(i, j, xDim)] = background;
+				if (cpt < 18) {
+					maskImage[k][x_new(i, j, xDim_ext)] = background;
 				}
 
 			}
@@ -243,35 +260,28 @@ bool dilatation3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim,
 	size_t i, j, k, cpt, dim2d = xDim * yDim;
 	size_t xDim_ext = xDim + 2, yDim_ext = yDim + 2, zDim_ext = zDim + 2, dim2d_ext = xDim_ext * yDim_ext;
 
-	dataType** maskImage = (dataType**)malloc(zDim * sizeof(dataType*));
-	for (k = 0; k < zDim; k++)
-		maskImage[k] = (dataType*)malloc(dim2d * sizeof(dataType));
-	if (maskImage == NULL) return false;
-	//Initialization
-	for (k = 0; k < zDim; k++) {
-		for (i = 0; i < xDim; i++) {
-			for (j = 0; j < yDim; j++) {
-				maskImage[k][x_new(i, j, xDim)] = background;
-			}
-		}
-	}
-
 	dataType** extendedImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
-	for (k = 0; k < zDim_ext; k++)
+	dataType** maskImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
+	for (k = 0; k < zDim_ext; k++) {
 		extendedImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
-	if (extendedImage == NULL) return false;
+		maskImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
+	}
+	if (extendedImage == NULL || maskImage == NULL) return false;
+
 	//Initialization
 	for (k = 0; k < zDim_ext; k++) {
 		for (i = 0; i < xDim_ext; i++) {
 			for (j = 0; j < yDim_ext; j++) {
 				extendedImage[k][x_new(i, j, xDim_ext)] = background;
+				maskImage[k][x_new(i, j, xDim_ext)] = background;
 			}
 		}
 	}
 	copyDataToExtendedArea(imageDataPtr, extendedImage, zDim, xDim, yDim);
 	reflection3D(extendedImage, zDim_ext, xDim_ext, yDim_ext);
+	copyDataToAnotherArray(extendedImage, maskImage, zDim_ext, xDim_ext, yDim_ext);
 
-	//neigbors check
+	//neigbors check ---> 26 neighbors
 	for (k = 1; k < zDim_ext - 1; k++) {
 		for (i = 1; i < xDim_ext - 1; i++) {
 			for (j = 1; j < yDim_ext - 1; j++) {
@@ -360,30 +370,129 @@ bool dilatation3D(dataType** imageDataPtr, const size_t xDim, const size_t yDim,
 				}
 
 				if (cpt > 0) {
-					maskImage[k - 1][x_new(i - 1, j - 1, xDim)] = object;
+					maskImage[k][x_new(i, j, xDim_ext)] = object;
 				}
 			}
 		}
 	}
-
-	//copy
-	for (k = 0; k < zDim; k++) {
-		for (i = 0; i < xDim; i++) {
-			for (j = 0; j < yDim; j++) {
-				imageDataPtr[k][x_new(i, j, xDim)] = maskImage[k][x_new(i, j, xDim)];
-			}
-		}
-	}
+	copyDataToReducedArea(imageDataPtr, maskImage, zDim, xDim, yDim);
 
 	//clean memory
-	for (k = 0; k < zDim; k++)
-		free(maskImage[k]);
-	free(maskImage);
-
-	for (k = 0; k < zDim + 2; k++)
+	for (k = 0; k < zDim_ext; k++) {
 		free(extendedImage[k]);
-	free(extendedImage);
+		free(maskImage[k]);
+	}
+	free(extendedImage); free(maskImage);
 
 	return true;
 }
 
+bool dilatation3dHeighteenNeigbours(dataType** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, dataType object, dataType background) {
+	if (imageDataPtr == NULL)
+		return false;
+
+	size_t i, j, k, cpt, dim2d = xDim * yDim;
+	size_t xDim_ext = xDim + 2, yDim_ext = yDim + 2, zDim_ext = zDim + 2, dim2d_ext = xDim_ext * yDim_ext;
+
+	dataType** extendedImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
+	dataType** maskImage = (dataType**)malloc(zDim_ext * sizeof(dataType*));
+	for (k = 0; k < zDim_ext; k++) {
+		extendedImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
+		maskImage[k] = (dataType*)malloc(dim2d_ext * sizeof(dataType));
+	}
+	if (extendedImage == NULL || maskImage == NULL) return false;
+	//Initialization
+	for (k = 0; k < zDim_ext; k++) {
+		for (i = 0; i < xDim_ext; i++) {
+			for (j = 0; j < yDim_ext; j++) {
+				extendedImage[k][x_new(i, j, xDim_ext)] = background;
+				maskImage[k][x_new(i, j, xDim_ext)] = background;
+			}
+		}
+	}
+	copyDataToExtendedArea(imageDataPtr, extendedImage, zDim, xDim, yDim);
+	reflection3D(extendedImage, zDim_ext, xDim_ext, yDim_ext);
+	copyDataToAnotherArray(extendedImage, maskImage, zDim_ext, xDim_ext, yDim_ext);
+
+	//neigbors check ---> 18 neigbors
+	for (k = 1; k < zDim_ext - 1; k++) {
+		for (i = 1; i < xDim_ext - 1; i++) {
+			for (j = 1; j < yDim_ext - 1; j++) {
+
+				cpt = 0;
+
+				if (extendedImage[k - 1][x_new(i - 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k - 1][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+
+				if (extendedImage[k][x_new(i - 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i - 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i - 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k][x_new(i + 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+
+				if (extendedImage[k + 1][x_new(i - 1, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i, j, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i, j + 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i + 1, j - 1, xDim_ext)] == object) {
+					cpt++;
+				}
+				if (extendedImage[k + 1][x_new(i + 1, j, xDim_ext)] == object) {
+					cpt++;
+				}
+
+				if (cpt > 0) {
+					maskImage[k][x_new(i, j, xDim_ext)] = object;
+				}
+
+			}
+		}
+	}
+	copyDataToReducedArea(imageDataPtr, maskImage, zDim, xDim, yDim);
+
+	for (k = 0; k < zDim_ext; k++) {
+		free(extendedImage[k]);
+		free(maskImage[k]);
+	}
+	free(extendedImage);
+	free(maskImage);
+
+	return true;
+}
