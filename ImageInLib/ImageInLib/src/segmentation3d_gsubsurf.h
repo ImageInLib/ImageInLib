f3a258bdb1740420071ff973b7c4b9dd3e55a48e
Konan Allaly <konan.allaly@tatramed.sk>
2022-12-23 10:38:39 +0100

    add of files and functions for gsubsurf

---
+++ ImageInLib/ImageInLib/src/segmentation3d_gsubsurf.h
@@ -0,0 +1,26 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma once
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "common_functions.h"
+#include "heat_equation.h"
+#include "filter_params.h"
+#include "segmentation3D_subsurf.h"
+
+	bool generalizedSubsurfSegmentation(Image_Data inputImageData, dataType** segFunct, Segmentation_Parameters segParameters, Filter_Parameters explicit_lhe_Parameters,
+		Point3D* centers, size_t no_of_centers, unsigned char* outputPathPtr, dataType a, dataType b);
+	bool generalizedGFunctionForImageToBeSegmented(Image_Data inputImageData, dataType** extendedCoefPtr, dataType** edgeGradientPtr, Gradient_Pointers GPtrs, Gradient_Pointers VPtrs,
+		Segmentation_Parameters segParameters, Filter_Parameters explicit_lhe_Parameters);
+	bool generalizedGaussSeidelCoefficients(dataType** extendedCoefPtr, Segment_Image_Data inputImageData, dataType** edgeGradientPtr,
+		Coefficient_Pointers CoefPtrs, Segmentation_Parameters segParameters);
+	bool generalizedSubsurfSegmentationTimeStep(dataType** prevSol_extPtr, dataType** gauss_seidelPtr, Segment_Image_Data inputImageData, Gradient_Pointers GPtrs, Gradient_Pointers VPtrs,
+		Segmentation_Parameters segParameters, Coefficient_Pointers CoefPtrs, Point3D* centers, size_t no_of_centers, dataType a, dataType b);
+
+#ifdef __cplusplus
+}
+#endif
