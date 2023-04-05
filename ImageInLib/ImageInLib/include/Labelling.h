258afb766dfb54f49e9b677107a8b7618a42d029
Konan Allaly <konan.allaly@tatramed.sk>
2023-04-05 14:58:25 +0200

    update labelling functions

--- ImageInLib/ImageInLib/include/Labelling.h
+++ ImageInLib/ImageInLib/include/Labelling.h
@@ -7,6 +7,10 @@ extern "C" {
 #include "../src/filter_params.h"
 #include "../src/heat_equation.h"
 
+	typedef struct {
+		size_t x, y;
+		bool label;
+	}point2dLabelling;
 
 	//for 2D image using 2D arrays
 	bool labelling2D(int** imageDataPtr, int** segmentedImage, bool** statusArray, const size_t xDim, const size_t yDim, int object);
@@ -15,6 +19,8 @@ extern "C" {
 	bool labelling3D(dataType** imageDataPtr, int** segmentedImage, bool** statusArray, const size_t xDim, const size_t yDim, const size_t zDim, dataType object);
 
 	bool regionGrowing(dataType** imageDataPtr, dataType** segmentedImage, bool** statusArray, const size_t xDim, const size_t yDim, const size_t zDim, dataType thres_min, dataType thres_max, Point3D* seedPoint);
+
+	bool new_labelling2D(dataType* imageDataPtr, dataType* segmentedImage, const size_t xDim, const size_t yDim, int object);
 	
 
 #ifdef __cplusplus
