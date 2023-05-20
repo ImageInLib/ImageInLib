77d606e72ee1cd2ccfa05493be79aadd75d67cf8
Konan Allaly <konan.allaly@tatramed.sk>
2023-05-20 16:50:16 +0200

    interface for manage file : template version

--- ImageInLib/ImageInLib/include/template_functions.h
+++ ImageInLib/ImageInLib/include/template_functions.h
@@ -1,3 +1,8 @@
+/*
+* Author: Konan ALLALY
+* Purpose: INFLANET project - Image Processing in Nuclear Medicine (2D/3D)
+* Language:  C and C++
+*/
 #pragma once
 
 #include<iostream>
@@ -7,13 +12,66 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef enum {
+	LOAD_DATA = 1,
+	STORE_DATA
+} Operation;
+
+//3D Image
+
 template <typename T>
-bool store3dRawData(T** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr);
+bool load3dArrayRAW(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr, bool revert);
+
+template <typename T>
+inline bool load3dArrayRAW(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr, bool revert)
+{
+	//checks if the memory was allocated
+	if (imageDataPtr == NULL || pathPtr == NULL) {
+		printf("Not initialized data \n");
+		return false;
+	}
+
+	size_t i, k;
+	FILE* file;
+	char rmode[4] = "rb";
+	const size_t pointsInSlice = width * length;
+
+	// Reading data file
+	if (fopen_s(&file, pathPtr, rmode) != 0) {
+		fprintf(stderr, "Error: Unable to open file %s\n\n", pathPtr);
+		return false;
+	}
+	else {
+		for (k = 0; k < height; k++) {
+			fread(imageDataPtr[k], sizeof(T), pointsInSlice, file);
+		}
+	}
+
+	const size_t dataSize = sizeof(T);
+	char* swapBuf = (char*)malloc(dataSize);
+
+	//revert byte
+	if (revert == true) {
+		for (k = 0; k < height; k++) {
+			for (i = 0; i < pointsInSlice; i++) {
+				revertBytesEx(&imageDataPtr[k][i], dataSize, swapBuf);
+			}
+		}
+	}
+
+	free(swapBuf);
+
+	fclose(file);
+	return true;
+}
+
+template <typename T>
+bool store3dRawData(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr);
 
 template<typename T>
-inline bool store3dRawData(T** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr)
+inline bool store3dRawData(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr)
 {
-	size_t i, j, k;
+	size_t k;
 	FILE* file;
 
 	//checks if the memory was allocated
@@ -24,9 +82,9 @@ inline bool store3dRawData(T** imageDataPtr, const size_t xDim, const size_t yDi
 	if (fopen_s(&file, pathPtr, "wb") != 0)
 		return false;
 
-	const size_t pointsInSlice = xDim * yDim;
+	const size_t pointsInSlice = length * width;
 
-	for (k = 0; k < zDim; k++)
+	for (k = 0; k < height; k++)
 	{
 		fwrite(imageDataPtr[k], sizeof(T), pointsInSlice, file);
 	}
@@ -36,11 +94,38 @@ inline bool store3dRawData(T** imageDataPtr, const size_t xDim, const size_t yDi
 	return true;
 }
 
-template <typename T>
-bool load3dArrayRAW(T** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr);
+template<typename T>
+bool manageRAWFile3D(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr, Operation operation, bool revert);
 
 template <typename T>
-inline bool load3dArrayRAW(T** imageDataPtr, const size_t xDim, const size_t yDim, const size_t zDim, const char* pathPtr)
+inline bool manageRAWFile3D(T** imageDataPtr, const size_t length, const size_t width, const size_t height, const char* pathPtr, Operation operation, bool revert) {
+
+	bool status = false; // Initial Status, only changed to true if the operation is successful
+
+	switch (operation)
+	{
+	case LOAD_DATA:
+		status = load3dArrayRAW<T>(imageDataPtr, length, width, height, pathPtr, revert);
+		break;
+	case STORE_DATA:
+		status = store3dRawData<T>(imageDataPtr, length, width, height, pathPtr);
+		break;
+	default:
+		break;
+	}
+
+	return status;
+}
+
+//===========================================================================
+
+//2D image
+
+template <typename T>
+bool load2dArrayRAW(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr, bool revert);
+
+template <typename T>
+inline bool load2dArrayRAW(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr, bool revert)
 {
 	//checks if the memory was allocated
 	if (imageDataPtr == NULL || pathPtr == NULL) {
@@ -48,11 +133,10 @@ inline bool load3dArrayRAW(T** imageDataPtr, const size_t xDim, const size_t yDi
 		return false;
 	}
 
-	size_t i, j, k, xd;
-	T value;
+	size_t i;
 	FILE* file;
 	char rmode[4] = "rb";
-	const size_t pointsInSlice = xDim * yDim;
+	const size_t pointsInSlice = length * width;
 
 	// Reading data file
 	if (fopen_s(&file, pathPtr, rmode) != 0) {
@@ -60,35 +144,30 @@ inline bool load3dArrayRAW(T** imageDataPtr, const size_t xDim, const size_t yDi
 		return false;
 	}
 	else {
-		for (k = 0; k < zDim; k++){
-			fread(imageDataPtr[k], sizeof(T), pointsInSlice, file);
-		}
+		fread(imageDataPtr, sizeof(T), pointsInSlice, file);
 	}
 
-	const size_t step = xDim * yDim;
 	const size_t dataSize = sizeof(T);
-	char* swapBuf = (char *)malloc(dataSize);
+	char* swapBuf = (char*)malloc(dataSize);
 
 	//revert byte
-	for (k = 0; k < zDim; k++) {
-		for (i = 0; i < step; i++) {
-			revertBytesEx(&imageDataPtr[k][i], dataSize, swapBuf);
+	if (revert == true) {
+		for (i = 0; i < pointsInSlice; i++) {
+			revertBytesEx(&imageDataPtr[i], dataSize, swapBuf);
 		}
 	}
 
 	free(swapBuf);
 
-
 	fclose(file);
 	return true;
 }
 
-//2D image...
 template <typename T>
-bool store2dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const char* pathPtr);
+bool store2dRawData(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr);
 
 template<typename T>
-inline bool store2dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim, const char* pathPtr)
+inline bool store2dRawData(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr)
 {
 	FILE* file;
 
@@ -100,11 +179,36 @@ inline bool store2dRawData(T* imageDataPtr, const size_t xDim, const size_t yDim
 	if (fopen_s(&file, pathPtr, "wb") != 0)
 		return false;
 
-	const size_t pointsInSlice = xDim * yDim;
+	const size_t pointsInSlice = length * width;
 
 	fwrite(imageDataPtr, sizeof(T), pointsInSlice, file);
 
 	fclose(file);
 
 	return true;
+}
+
+
+template<typename T>
+bool manageRAWFile2D(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr, Operation operation, bool revert);
+
+template <typename T>
+inline bool manageRAWFile2D(T* imageDataPtr, const size_t length, const size_t width, const char* pathPtr, Operation operation, bool revert) {
+
+	bool status = false; // Initial Status, only changed to true if the operation is successful
+
+	switch (operation)
+	{
+	case LOAD_DATA:
+		status = load2dArrayRAW<T>(imageDataPtr, length, width, pathPtr, revert);
+		break;
+	case STORE_DATA:
+		status = store2dRawData<T>(imageDataPtr, length, width, pathPtr);
+		break;
+
+	default:
+		break;
+	}
+
+	return status;
 }
