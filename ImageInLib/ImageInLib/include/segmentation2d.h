3e26b0c85fa3a7d8c34bd850b22b49cb4a270de4
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-04 11:43:09 +0200

    add files for 2d segmentations

---
+++ ImageInLib/ImageInLib/include/segmentation2d.h
@@ -0,0 +1,18 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma once
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "common_functions.h"
+#include "heat_equation.h"
+#include "filter_params.h"
+
+	dataType min(dataType a, dataType b);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ ImageInLib/ImageInLib/src/segmentation2d.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "segmentation2d.h"
+
+dataType min(dataType a, dataType b) {
+	if (a > b) {
+		return b;
+	}
+	else {
+		return a;
+	}
+}
+
