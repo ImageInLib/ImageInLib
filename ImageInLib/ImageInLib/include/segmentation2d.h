4a80adca41a13e4e2d6299e4bb9f0ebbcc9bd7d1
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-04 12:04:17 +0200

    add function for generation 2d initial segmentation function and rescall to range 0-1

--- ImageInLib/ImageInLib/include/segmentation2d.h
+++ ImageInLib/ImageInLib/include/segmentation2d.h
@@ -10,8 +10,13 @@ extern "C" {
 #include "common_functions.h"
 #include "../src/heat_equation.h"
 #include "../src/filter_params.h"
+#include "../distanceForPathFinding.h"
 
 	dataType min(dataType a, dataType b);
+
+	bool rescaleToZeroOne2d(dataType* imageDataPtr, const size_t height, const size_t width);
+
+	bool generateInitialSegmentationFunction(dataType* imageDataPtr, const size_t height, const size_t width, point2D* center, dataType v, dataType R);
 
 #ifdef __cplusplus
 }
--- ImageInLib/ImageInLib/src/segmentation2d.cpp
+++ ImageInLib/ImageInLib/src/segmentation2d.cpp
@@ -10,3 +10,56 @@ dataType min(dataType a, dataType b) {
 	}
 }
 
+bool rescaleToZeroOne2d(dataType* imageDataPtr, const size_t height, const size_t width)
+{
+	//check if the memory was allocated successfully
+	if (imageDataPtr == NULL)
+		return false;
+
+	size_t i;
+	dataType max = 0, min = 100000, quotient, offset;
+
+	//Determine minimum and maximum value
+	for (i = 0; i < height * width; i++) {
+		if (imageDataPtr[i] < min)
+			min = imageDataPtr[i];
+		if (imageDataPtr[i] > max)
+			max = imageDataPtr[i];
+	}
+
+	quotient = (dataType)1. / (max - min);
+	offset = min * quotient;
+	//Rescale values to interval (0, 1)
+	for (i = 0; i < height * width; i++) {
+		imageDataPtr[i] = (quotient * imageDataPtr[i] - offset);
+	}
+
+	return true;
+}
+
+bool generateInitialSegmentationFunction(dataType* imageDataPtr, const size_t height, const size_t width, point2D* center, dataType v, dataType R)
+{
+	size_t i, j;
+	int dx, dy;
+	dataType norm_of_distance = 0.0, new_value = 0.0;
+
+	if (imageDataPtr == NULL)
+		return;
+
+	for (i = 0; i < height; i++) {
+		dx = i - center->x;
+		for (j = 0; j < width; j++) {
+			dy = j - center->y;
+			norm_of_distance = sqrt(dx * dx + dy * dy);
+			new_value = (dataType)((1.0 / (norm_of_distance + v)) - (1.0 / (R + v)));
+			if (norm_of_distance > R) {
+				imageDataPtr[x_new(i, j, height)] = 0;
+			}
+			else {
+				imageDataPtr[x_new(i, j, height)] = new_value;
+			}
+		}
+	}
+	rescaleToZeroOne2d(imageDataPtr, height, width);
+}
+
