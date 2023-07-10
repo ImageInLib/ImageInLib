c4c5d68f3f2388d354e6c1822fc02ec6c850f12f
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-10 17:07:11 +0200

    add segmentation2d.c

--- ImageInLib/ImageInLib/include/segmentation2d.h
+++ ImageInLib/ImageInLib/include/segmentation2d.h
@@ -3,16 +3,11 @@ extern "C" {
 #endif
 
 #pragma once
-//#include<iostream>
-#include <stdio.h>
-#include <string>
-
 #include "common_functions.h"
+#include "../src/data_storage.h"
 #include "../src/heat_equation.h"
 #include "../src/filter_params.h"
-#include "../distanceForPathFinding.h"
 #include "../src/segmentation3D_subsurf.h"
-#include"template_functions.h"
 
 	typedef struct
 	{
@@ -22,13 +17,18 @@ extern "C" {
 		dataType* South;
 	} neighPtrs;
 
-	dataType min(dataType a, dataType b);
+	typedef struct
+	{
+		size_t x, y;
+	} point2d;
+
+	//dataType min(dataType a, dataType b);
 
 	dataType l2norm(dataType* arrayPtr1, dataType* arrayPtr2, const size_t height, const size_t width, dataType h);
 
 	bool rescaleToZeroOne2d(dataType* imageDataPtr, const size_t height, const size_t width);
 
-	bool generateInitialSegmentationFunction(dataType* imageDataPtr, const size_t height, const size_t width, point2D* center, dataType v, dataType R);
+	bool generateInitialSegmentationFunction(dataType* imageDataPtr, const size_t height, const size_t width, point2d* center, dataType v, dataType R);
 
 	bool set2dDirichletBoundaryCondition(dataType* imageDataPtr, const size_t height, const size_t width);
 
