6ad45b084d5150561750baf572ff63a9182c4832
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-11 16:54:36 +0200

    interpolate to real dimension

--- ImageInLib/ImageInLib/src/imageInterpolation.c
+++ ImageInLib/ImageInLib/src/imageInterpolation.c
@@ -1,3 +1,4 @@
+#include"../src/data_storage.h"
 #include "interpolations.h"
 #include "imageInterpolation.h"
 #include<math.h>
@@ -36,7 +37,8 @@ bool nearestNeighborInterpolation(dataType** originalImage, dataType** newImage,
             kn = kn + 1;
         } while (k_int < k2);
     }
-    return false;
+
+    return true;
 }
 
 bool linear2dInterpolation(dataType** originalImage, dataType** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight, dataType originalSpacing, dataType newSpacing)
@@ -115,6 +117,80 @@ bool upSampling(dataType** originalImage, dataType** newImage, size_t length, si
             }
         }
     }
+
+    return true;
+}
+
+bool interpolateToRealDimension(patientImageData imageSrc, const char* outputPathPtr) {
+    
+    if (imageSrc.dataPtr == NULL)
+        return false;
+
+    size_t k, i, j;
+    dataType k_real = 0, i_real = 0, j_real = 0;
+    size_t k_int = 0, i_int = 0, j_int = 0;
+
+    size_t height = imageSrc.height, length = imageSrc.length, width = imageSrc.width;
+    const size_t real_height = (size_t)(height * imageSrc.toRealCoordinates.sz);
+    const size_t real_length = (size_t)(length * imageSrc.toRealCoordinates.sx);
+    const size_t real_width = (size_t)(width * imageSrc.toRealCoordinates.sy);
+
+    dataType** destImage = (dataType**)malloc(sizeof(dataType*) * real_height);
+    for (k = 0; k < real_height; k++) {
+        destImage[k] = (dataType*)malloc(sizeof(dataType) * real_length * real_width);
+    }
+    if (destImage == NULL)
+        return false;
+
+    for (k = 0; k < height; k++) {
+
+        k_real = k * imageSrc.toRealCoordinates.sz;
+
+        dataType diffZ1 = abs(k_real - k);
+        dataType diffZ2 = abs(k_real - (k + 1));
+        if (diffZ1 > diffZ2) {
+            k_int = k;
+        }
+        else {
+            k_int = k + 1;
+        }
+
+        for (i = 0; i < length; i++) {
+
+            i_real = i * imageSrc.toRealCoordinates.sx;
+            dataType diffX1 = abs(i_real - i);
+            dataType diffX2 = abs(i_real - (i + 1));
+            if (diffX1 > diffX2) {
+                i_int = i;
+            }
+            else {
+                i_int = i + 1;
+            }
+
+            for (j = 0; j < width; j++) {
+
+                j_real = j * imageSrc.toRealCoordinates.sy;
+                dataType diffY1 = abs(j_real - j);
+                dataType diffY2 = abs(j_real - (j + 1));
+                if (diffY1 > diffY2) {
+                    j_int = j;
+                }
+                else {
+                    j_int = j + 1;
+                }
+                destImage[k_int][x_new(i_int, j_int, real_length)] = imageSrc.dataPtr[k][x_new(i, j, length)];
+            }
+        }
+    }
+
+    printf("The dimensions of new image are \n Length = %zd, Width = %zd, Heigth = %zd", real_length, real_width, real_height);
+    Storage_Flags flags = { false, false };
+    store3dDataArrayD(destImage, real_length, real_width, real_height, outputPathPtr, flags);
+    
+    for (k = 0; k < real_height; k++) {
+        free(destImage[k]);
+    }
+    free(destImage);
 
     return true;
 }
--- ImageInLib/ImageInLib/src/imageInterpolation.h
+++ ImageInLib/ImageInLib/src/imageInterpolation.h
@@ -7,16 +7,31 @@ extern "C" {
 #include "common_functions.h"
 #include "transformation.h"
 
+	typedef struct {
+		dataType sx, sy, sz;
+	} Spacing;
 
+	typedef struct {
+		Point3D origin;
+		size_t height, width, length;
+		dataType** dataPtr;
+		Spacing toRealCoordinates;
+	} patientImageData;
+
+	//Interpolation regarding z-Direction
 	bool nearestNeighborInterpolation(dataType ** originalImage, dataType ** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight,
 		                  dataType originalSpacing, dataType newSpacing);
 
 	bool linear2dInterpolation(dataType** originalImage, dataType** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight,
 						  dataType originalSpacing, dataType newSpacing);
+	//====================
 
 	bool downSampling(dataType** originalImage, dataType** newImage, size_t length, size_t width, size_t height);
 
 	bool upSampling(dataType** originalImage, dataType** newImage, size_t length, size_t width, size_t height);
+
+	//Interpolation in all directions
+	bool interpolateToRealDimension(patientImageData imageSrc, const char * outputPathPtr);
 
 #ifdef __cplusplus
 }
