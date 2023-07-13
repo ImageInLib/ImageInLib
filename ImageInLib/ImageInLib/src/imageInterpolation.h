ff4e2ace92f3ec292a4b86cfa4c3b783a77ba350
Konan Allaly <konan.allaly@tatramed.sk>
2023-07-13 23:34:53 +0200

    resize 2d image by nearest neighbor interpolation

--- ImageInLib/ImageInLib/src/imageInterpolation.h
+++ ImageInLib/ImageInLib/src/imageInterpolation.h
@@ -9,11 +9,11 @@ extern "C" {
 #include "transformation.h"
 
 	//Interpolation regarding z-Direction
-	bool nearestNeighborInterpolation(dataType ** originalImage, dataType ** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight,
-		                  dataType originalSpacing, dataType newSpacing);
+	bool nearestNeighborInterpolation(dataType** originalImage, dataType** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight,
+		dataType originalSpacing, dataType newSpacing);
 
 	bool linear2dInterpolation(dataType** originalImage, dataType** newImage, size_t imageLength, size_t imageWidth, size_t imageHeight,
-						  dataType originalSpacing, dataType newSpacing);
+		dataType originalSpacing, dataType newSpacing);
 
 	bool downSampling(dataType** originalImage, dataType** newImage, size_t length, size_t width, size_t height);
 
@@ -24,13 +24,11 @@ extern "C" {
 	//Structure to handle image spacing
 	typedef struct {
 		dataType sx, sy, sz;
-	} Spacing3D;
+	} voxelSpacing;
 
-	//Structure to handle image coordinates
-	//We need new structure because coordinates are array indexes (integer type is needed)
 	typedef struct {
-		size_t x, y, z;
-	} imgPoint3D;
+		Point3D v1, v2, v3;
+	}orientationMatrix;
 
 	/*
 	* Get real world cordinate from image coordinate
@@ -38,14 +36,19 @@ extern "C" {
 	* realOrigin : image origin in real world
 	* imageSpacing : - distance between voxels in x and y direction
 	*                - distance between slices
-	* orientation : IJK(1,1,1), RAS(-1,-1,1) or LPS(1,-1,-1)
+	* orientation : IJK((1, 0, 0),(0,1,0),(0,0,1)), RAS((-1,0,0),(0,-1,0),(0,0,1))
+	* or LPS((0,0,1),(0,-1,0),(0,0,-))
 	*/
-	Point3D imageCoordToRealCoord(imgPoint3D srcPoint, Point3D realOrigin, Spacing3D imageSpacing, Point3D orientation);
+	Point3D imageCoordToRealCoord(Point3D srcPoint, Point3D realOrigin, voxelSpacing imageSpacing, orientationMatrix orientation);
 
 	//Get image coordinate from real coordinate
-	imgPoint3D realCoordToImageCoord(Point3D srcPoint, Point3D realOrigin, Spacing3D imageSpacing, Point3D orientation);
+	//Point3D realCoordToImageCoord(Point3D srcPoint, Point3D realOrigin, voxelSpacing3D imageSpacing, orientationMatrix orientation);
 
-	bool resizeImage(dataType* oldImage, dataType* newImage);
+	typedef struct {
+		dataType sx, sy;
+	} pixelSpacing;
+
+	bool resizeImage(Image_Data2D oldImage, Image_Data2D newImage);
 
 #ifdef __cplusplus
 }
